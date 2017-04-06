63e82384da79dfd6257dc3e9a9df0685970d6ea5
pandu <pandubalraj@gmail.com>
2017-04-06 09:33:23 +0530

    changes for name

--- facebook_bot.js
+++ facebook_bot.js
@@ -110,10 +110,9 @@ var welcome_message = ['^hi$','^start_payload$','^hello$','^start$','^hey$','^wh
 controller.hears(welcome_message, 'message_received,facebook_postback', function(bot, message) {
     bot.startConversation(message, function(err, convo) {
         
-
+       
         
-        
-        convo.say('Hi'+test_name+' , I am BhubaBot. Nice to meet you ☺ ')
+        convo.say('Hi, I am BhubaBot. Nice to meet you ☺ ')
         convo.say('Did you know, I am from Planet Mars - Elon Musk pulled me back in a SpaceX Falcon Rocket  ');
         convo.say('You know why? To give you Geeks some valuable info  and also, to savor the lovely Bhubaneshwar delicacies- Especially the Rosgollas. OK. ');
         convo.say('Now that I know a little about Bhubaneshwar and also have managed to grasp a little bit of English, I might be able to help you.');

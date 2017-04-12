d0307e57a55c5ccc467a24877ad3ae37977684fa
neetucompiler <neetumishra231093@gmail.com>
2017-04-12 14:05:26 +0530

    Update facebook_bot.js

--- facebook_bot.js
+++ facebook_bot.js
@@ -3,15 +3,6 @@ var os = require('os')
 var localtunnel = require('localtunnel')
 var opn = require('opn')
 var request = require('request')
-var express = require('express')
-var app = express()
-var fs = require('fs')
-
-app.get('/privacy_policy', function (req, res) {
-fs.readFile(__dirname + '/privacy_policy.html', 'utf8', function(err, text){
-        res.send(text);
-    });
-})
 
 // controller bot definition
 var controller = Botkit.facebookbot({
